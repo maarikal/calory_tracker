5712b6ef3ce91f39db4290e8309715973e97be15
Maarika Lukk <maarika.lukk@khk.ee>
2022-02-23 15:37:09 +0200

    Added item to the UI list

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const ItemCtrl =(function () {
     // Data Structure
     const data = {
         items: [
-            {id: 0, name: "Šnitsel", calories: 1000},
-            {id: 1, name: "Küpsis", calories: 450},
-            {id: 2, name: "Keedumuna", calories:300}
+           // {id: 0, name: "Šnitsel", calories: 1000},
+            // {id: 1, name: "Küpsis", calories: 450},
+            // {id: 2, name: "Keedumuna", calories:300}
         ],
         total: 0
     }
@@ -84,6 +84,26 @@ const UICtrl = (function (){
                 name: document.querySelector(UISelectors.itemNameInput).value,
                 calories: document.querySelector(UISelectors.itemCaloriesInput).value
             }
+        },
+        addListItem: function (item) {
+            // create li element
+            const li = document.createElement('li');
+            // add class
+            li.className = 'collection-item';
+            // add ID
+            li.id = `item-${item.id}`;
+            // add HTML
+            li.innerHTML = `<strong>${item.name}: </strong>
+                <em>${item.calories} Calories </em>
+                <a href='#' class='secondary-content'>
+                    <i class='edit-item fa fa-pencil'> </i>
+                </a>`;
+            // insert item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+        },
+        clearInput: function () {
+            document.querySelector(UISelectors.itemNameInput).value = '';
+            document.querySelector(UISelectors.itemCaloriesInput).value = ''
         }
     }
 })();
@@ -105,7 +125,9 @@ const App = (function (ItemCtrl,  UICtrl) {
             // check for name and calorie input
             if(input.name !== '' && input.calories !== '') {
                 const newItem = ItemCtrl.addItem(input.name, input.calories)
-                console.log(newItem)
+                UICtrl.addListItem(newItem)
+                // clear fields
+                UICtrl.clearInput();
             }
             console.log(input)
             event.preventDefault()
